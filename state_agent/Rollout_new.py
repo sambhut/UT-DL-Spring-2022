1543deb12d4363c2a7da8a88f14e9785eff22f76
rohitpoduri12 <poduri.rohit@gmail.com>
2023-04-29 19:43:57 -0700

    Fixed all the bugs in Rollout code. Now code is collecting data correctly

--- state_agent/Rollout_new.py
+++ state_agent/Rollout_new.py
@@ -15,19 +15,12 @@ ray_init_done = 0
 
 pystk_init_done = False
 
-opponents_str = ["Geoffrey", "ImJurgen()", "Jurgen()", "Yann()", "Yoshua()", "AIRunner()"]
-
 class AIRunner:
-    #agent_type = 'state'
-    #is_ai = True
     def new_match(self, team: int, num_players: int) -> list:
         pass
 
     def act(self, player_state, opponent_state, world_state):
         return []
-
-    #def info(self):
-    #    return RunnerInfo('state', None, 0)
 
 def to_native(o):
     # Super obnoxious way to hide pystk
@@ -68,34 +61,60 @@ class Rollout_new:
         # fire up pystk
         graphics_config = pystk.GraphicsConfig.none()
 
+        # init pystk only if it hasn't been initialized before
         if pystk_init_done == False:
             pystk_init_done = True
             pystk.init(graphics_config)
 
+        # list of opponents to get rollouts against
         opponents = [Geoffrey(), ImJurgen(), Jurgen(), Yann(), Yoshua(), AIRunner()]
 
+        # set number of players in each team
         self.num_player = num_player
 
+        # If opponent_id was not provided, then set the opponent_id to AI
+        if opponent_id == -1:
+            opponent_id = len(opponents) - 1
+
+        # For the opponent_id we received, we calculate the opponent number by taking a mod
+        # with number of opponents. So opponent_no is always 0-5. So Geoffrey() will be
+        # selected for opponent_id 0, 6, 12 and so on.
         opponent_no = opponent_id % len(opponents)
 
+        # This variable represents the team number for AI. If it is -1 it means both team0
+        # and team1 are player agents
         AIteam = -1
 
+        # By default player is team0
         self.player_team = 0
 
+        # If we are training we want to collect multiple trajectories. So based on the
+        # opponent_id we set player to team0 or team1. In the current logic of the code,
+        # for opponent_no 0-5, player is team0 and opponent is team1. For any values beyond
+        # 5, player is team1 and opponent is team0. I wanted to extend my logic to something
+        # like this- opponent_id- 0-5 player is team0, opponent_id 6-11 player is team1,
+        # 12-17 player is team0 and so on (This logic is not implemented as I didn't get time).
+        # CURRENTLY FOR opponent_id 0-5 PLAYER IS TEAM0, AND opponent_id >= 6, PLAYER IS TEAM1.
         if train is True:
 
             if opponent_id < len(opponents):
                 team0_updated = team0
                 team1_updated = opponents[opponent_no]
+                # If opponent_no = 5, then opponent is AI (from the opponents list)
                 AIteam = 1 if opponent_no == (len(opponents)-1) else -1
+                # Player is team 0
                 self.player_team = 0
 
+            # The second case (opponent_id >=6, player is always team1)
             else:
                 team0_updated = opponents[opponent_no]
                 team1_updated = team0
+                # If opponent_no = 5, then opponent is AI (from the opponents list)
                 AIteam = 0 if opponent_no == (len(opponents)-1) else -1
+                # Player is team 1
                 self.player_team = 1
 
+        # When not training, use team0 and team as usual (player is team 0 and AI is team 1)
         else:
             team0_updated = team0
             team1_updated = team1
@@ -110,8 +129,6 @@ class Rollout_new:
         print("player team is ", self.player_team)
 
         # set teams for a new match
-        #team0_cars = self._g(self._r(team0.new_match)(0, num_player))
-        #team1_cars = self._g(self._r(team1.new_match)(1, num_player))
         team0_cars = team0_updated.new_match(0, num_player) or ['tux']
         team1_cars = team1_updated.new_match(1, num_player) or ['tux']
 
@@ -137,11 +154,12 @@ class Rollout_new:
             race_config.players.append(PlayerConfig(controller=controller0, team=0, kart=team0_cars[i % len(team0_cars)]))
             race_config.players.append(PlayerConfig(controller=controller1, team=1, kart=team1_cars[i % len(team1_cars)]))
 
-        #Start the match
+        # Start the match
         self.race = pystk.Race(race_config)
         self.race.start()
         self.race.step()
 
+        # Set team0 and team1 correctly
         self.team0 = team0_updated
         self.team1 = team1_updated
 
@@ -152,7 +170,7 @@ class Rollout_new:
         state = pystk.WorldState()
         state.update()
 
-        # Add some randomness to the starting location
+        # When train is True, add some randomness to the puck/soccer ball's starting position
         if train is True :
             rand3 = random.uniform(-10, 10)
             rand4 = random.uniform(-10, 10)
@@ -162,10 +180,12 @@ class Rollout_new:
                                 [initial_ball_velocity[0], 0, initial_ball_velocity[1]],[initial_angular_velocity[0] , 0, initial_angular_velocity[1]])
             state.update()
 
+        # Storing these values for velocity calculations. Set initial values to 0.
         old_puck_center = torch.Tensor([0, 0])
         old_kart_center1 = torch.Tensor([0, 0])
         old_kart_center2 = torch.Tensor([0, 0])
 
+        # Start iterating over all frames
         for it in range(max_frames):
             #print("soccer ball location before update is ", to_native(state.soccer)['ball']['location'])
             state.update()
@@ -174,58 +194,66 @@ class Rollout_new:
             team0_state = [to_native(p) for p in state.players[0::2]]
             team1_state = [to_native(p) for p in state.players[1::2]]
             soccer_state = to_native(state.soccer)
+
+            # Collect appropriate data
             if self.player_team == 0:
-                team_state = team0_state
-                opp_state = team1_state
+                agent_data = {'player_state': team0_state, 'opponent_state': team1_state, 'soccer_state': soccer_state}
+                player_team_state = team0_state
+                opponent_team_state = team1_state
             else:
-                team_state = team1_state
-                opp_state = team0_state
+                agent_data = {'player_state': team1_state, 'opponent_state': team0_state, 'soccer_state': soccer_state}
+                player_team_state = team1_state
+                opponent_team_state = team0_state
 
-            agent_data = {'player_state': team_state, 'opponent_state': opp_state, 'soccer_state': soccer_state}
-
-            # print some data every 100 frames
-            if it<10 and train is True:
+            # print some data for the first 10 frames when train is False (change as needed)
+            if (it < 10) and train is False:
                 print("train is ", train)
                 print("iteration {%d} / {%d}" % (it, max_frames))
-                print("player kart0 location is ", team_state[0]["kart"]["location"])
-                print("player kart1 location is ", team_state[1]["kart"]["location"])
-                print("opponent kart0 is ", opp_state[0]["kart"]["location"])
-                print("opponent kart1 is ", opp_state[1]["kart"]["location"])
+                print("player kart0 location is ", player_team_state[0]["kart"]["location"])
+                print("player kart1 location is ", player_team_state[1]["kart"]["location"])
+                print("opponent kart0 is ", opponent_team_state[0]["kart"]["location"])
+                print("opponent kart1 is ", opponent_team_state[1]["kart"]["location"])
                 print("After soccer ball location is ", soccer_state['ball']['location'])
 
-            # Have each team produce actions (in parallel)
-            team_can_act = True  # True for now, or we can use _check function in runner
-            opp_can_act = True
-            if team_can_act:
-                team_actions_delayed = self._r(self.team0.act)(team_state, opp_state, soccer_state)
+            # Not removing to avoid any issues
+            t0_can_act = True
+            t1_can_act = True
+            if t0_can_act:
+                team0_actions_delayed = self._r(self.team0.act)(team0_state, team1_state, soccer_state)
 
-            if opp_can_act:
-                opp_actions_delayed = self._r(self.team1.act)(opp_state, team_state, soccer_state)
+            if t1_can_act:
+                team1_actions_delayed = self._r(self.team1.act)(team1_state, team0_state, soccer_state)
 
-            team_actions = team_actions_delayed if team_can_act else None
-            opp_actions = opp_actions_delayed if opp_can_act else None
+            team0_actions = team0_actions_delayed if t0_can_act else None
+            team1_actions = team1_actions_delayed if t1_can_act else None
 
             # Assemble the actions
             actions = []
             for i in range(self.num_player):
-                a0 = team_actions[i] if team_actions is not None and i < len(team_actions) else {}
-                a1 = opp_actions[i] if opp_actions is not None and i < len(opp_actions) else {}
+                a0 = team0_actions[i] if team0_actions is not None and i < len(team0_actions) else {}
+                a1 = team1_actions[i] if team1_actions is not None and i < len(team1_actions) else {}
                 actions.append(a0)
                 actions.append(a1)
 
             if record_fn:
-                self._r(record_fn)(team_state, opp_state, soccer_state=soccer_state, actions=actions)
+                self._r(record_fn)(team0_state, team1_state, soccer_state=soccer_state, actions=actions)
 
-
-            agent_data['action0'] = team_actions[0]
-            agent_data['action1'] = team_actions[1]
+            # Collect appropriate data
+            if self.player_team == 0:
+                agent_data['action0'] = team0_actions[0]
+                agent_data['action1'] = team0_actions[1]
+                current_kart_center1 = torch.tensor(team0_state[0]["kart"]["location"], dtype=torch.float32)[[0, 2]]
+                current_kart_center2 = torch.tensor(team0_state[1]["kart"]["location"], dtype=torch.float32)[[0, 2]]
+            else:
+                agent_data['action0'] = team1_actions[0]
+                agent_data['action1'] = team1_actions[1]
+                current_kart_center1 = torch.tensor(team1_state[0]["kart"]["location"], dtype=torch.float32)[[0, 2]]
+                current_kart_center2 = torch.tensor(team1_state[1]["kart"]["location"], dtype=torch.float32)[[0, 2]]
 
             self.race.step([pystk.Action(**a) for a in actions])
 
             # Gather velocity of puck and karts
             current_puck_center = torch.tensor(soccer_state['ball']['location'], dtype=torch.float32)[[0, 2]]
-            current_kart_center1 = torch.tensor(team_state[0]["kart"]["location"], dtype=torch.float32)[[0, 2]]
-            current_kart_center2 = torch.tensor(team_state[1]["kart"]["location"], dtype=torch.float32)[[0, 2]]
             agent_data['ball_velocity'] = current_puck_center - old_puck_center
             agent_data['kart_velocity'] = []
             agent_data['kart_velocity'].append(current_kart_center1 - old_kart_center1)
@@ -244,6 +272,8 @@ class Rollout_new:
 
         return data
 
+# This function is to be called only when training. It can change team0 and team1
+# based on the value of i in __init__ and randomize puck position based in __call__
 def rollout_many(many_agents, **kwargs):
     data = []
     for i, agent in enumerate(many_agents):
@@ -268,7 +298,7 @@ if __name__ == "__main__":
     if record_video:
         recorder = recorder & VideoRecorder(video_name)
 
-    rollout = Rollout_new(team0=team0, team1=team1, num_player=num_player, use_ray=use_ray, train=False)
+    rollout = Rollout_new(team0=team0, team1=team1, num_player=num_player, train=False)
 
     rollout.__call__(use_ray=use_ray, record_fn=recorder)
 
