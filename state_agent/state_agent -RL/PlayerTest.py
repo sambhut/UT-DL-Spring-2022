fbe16b0f3f6c2ea6b58cc3de26af8e5023661972
maniratna-g <122002020+maniratna-g@users.noreply.github.com>
2023-04-30 01:01:37 -0700

    Update PlayerTest.py

--- state_agent/state_agent -RL/PlayerTest.py
+++ state_agent/state_agent -RL/PlayerTest.py
@@ -1,18 +1,9 @@
-from torch.distributions import Bernoulli
-
-from state_agent import Team
+from state_agent.player import Team
 from state_agent.custom_runner import record_video, record_state, record_manystate
 from state_agent.planner import Planner
 import torch
-import torch
-import pystk
-from torch.distributions import Bernoulli
-from state_agent.planner import network_features
-import torch
 import numpy as np
 from os import path
-import numpy as np
-import torch
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -20,17 +11,8 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 if __name__ == "__main__":
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    player1_net = Planner(21, 64, 6).to(device)
-    player2_net = Planner(21, 64, 6).to(device)
+    player1_net = Planner(21, 32, 6).to(device)
     dic1 = torch.load('player1_action_model.pt')
-    dic2 = torch.load('player2_action_model.pt')
     player1_net.load_state_dict(dic1)
-    player2_net.load_state_dict(dic2)
-    action_net = Team(player1_net,player2_net)
+    action_net = Team(player1_net,player1_net)
     record_video(action_net)
-
-
-
-
-
-
