ef2c24015ad892b712bc1bbf5683ed759ab1a29d
maniratna-g <122002020+maniratna-g@users.noreply.github.com>
2023-04-30 01:02:18 -0700

    Update critic.py

--- state_agent/state_agent -RL/critic.py
+++ state_agent/state_agent -RL/critic.py
@@ -4,13 +4,11 @@ class ValueNetwork(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(ValueNetwork, self).__init__()
         self.fc1 = torch.nn.Linear(input_dim, hidden_dim)
-        self.bn1 = torch.nn.BatchNorm1d(hidden_dim)
-        self.fc2 = torch.nn.Linear(hidden_dim, 1)
+        self.fc2 = torch.nn.Linear(hidden_dim, 128)
+        self.fc3 = torch.nn.Linear(128, 1)
+        self.relu = torch.nn.ReLU()
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.bn1(x)
-        x = torch.relu(x)
-        x = self.fc2(x)
-        return x
-
+        x = torch.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        return self.fc3(x)
